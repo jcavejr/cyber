ef0efaf267ef93add60d34d0ac8f8a2931c5d1fe
Jeff Cave <jcavejr@gmail.com>
2018-09-20 15:07:56 -0400

    Added repeated connect for sender

--- RunSender.java
+++ RunSender.java
@@ -1,8 +1,14 @@
 import java.io.*;
 
 class RunSender {
+    private static int PORT_NUMBER;
     public static void main(String[] args) {
-        Sender mySender = new Sender();
+        if (args.length == 1) {
+            PORT_NUMBER = Integer.parseInt(args[0]);
+        } else {
+            PORT_NUMBER = 5000;
+        }
+        Sender mySender = new Sender(PORT_NUMBER);
         System.out.println("Connected to: " + mySender);
         String data = "This is a test string";
         mySender.sendObjectToStream(data);
--- Sender.java
+++ Sender.java
@@ -3,11 +3,12 @@ import java.io.*;
 
 public class Sender {
     private Socket serverPort;
-    private final int PORT_NUMBER = 5000;
+    // private final int PORT_NUMBER = 5000;
     private ObjectOutputStream stream;
+    private final int MAX_TRIES = 10;
 
-    public Sender() {
-        serverPort = openSocket();
+    public Sender(int portNumber) {
+        serverPort = openSocket(portNumber);
         try {
             stream = new ObjectOutputStream(serverPort.getOutputStream());
         } catch (IOException ex) {
@@ -25,18 +26,30 @@ public class Sender {
         }
     }
 
-    private Socket openSocket() {
-        try {
-            InetAddress localHost = InetAddress.getLocalHost();
-            System.out.println(localHost);
-            return new Socket(localHost, PORT_NUMBER);
-        } catch (UnknownHostException ex) {
-            ex.printStackTrace();
-            System.exit(1);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            System.exit(1);
+    private Socket openSocket(int portNumber) {
+        int tries = 0;
+        while (tries < MAX_TRIES) {
+            try {
+                System.out.println("Attempting to connect.. [" + tries + "/" + MAX_TRIES + "]");
+                InetAddress localHost = InetAddress.getLocalHost();
+                System.out.println(localHost);
+                return new Socket(localHost, portNumber);
+            } catch (UnknownHostException ex) {
+                ex.printStackTrace();
+                System.exit(1);
+            } catch (IOException ex) {
+                System.out.println("Going to sleep..");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+                tries++;
+            }
         }
+        System.err.println("Error: Tried connecting " + tries + " times but could not connect.");
+        System.exit(1);
         return null;
     }
 
